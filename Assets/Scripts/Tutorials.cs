793b77427354edb5da275dab9c286292c49a8261
jsj5909 <jsj5909@hotmail.com>
2021-04-14 15:20:39 -0700

    tutorial panel script complete

---
+++ Assets/Scripts/Tutorials.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorials : MonoBehaviour
+{
+    [SerializeField] bool tutorialsOn = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!tutorialsOn)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OKPressed()
+    {
+
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}

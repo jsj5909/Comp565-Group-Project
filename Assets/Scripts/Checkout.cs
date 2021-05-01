6a242e1b6649d8b855797f57c37351f906cfdc5b
jsj5909 <jsj5909@hotmail.com>
2021-04-30 17:11:48 -0700

    initial Checkout Panel UI completed

---
+++ Assets/Scripts/Checkout.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkout : MonoBehaviour
+{
+    [SerializeField] GameObject checkoutPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag.Equals("Player"))
+        {
+            checkoutPanel.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/CheckoutPanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CheckoutPanel : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI storeName;
+    [SerializeField] TextMeshProUGUI descriptionText;
+
+    private PlayerController player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        descriptionText.text = "You survived another day at " + storeName.text.Substring(9);
+
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        player.SetCanMove(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextLevelButtonPressed()
+    {
+        int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+
+       
+        SceneManager.LoadScene(nextScene);
+    }
+
+}
